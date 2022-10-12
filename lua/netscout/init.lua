1f52b53bc092fbb061f3533b76ce61ca10d8825a
Massimiliano Frigieri <frigierim@netscout.com>
2022-10-12 10:12:25 +0200

    Made configuration specific per workspace

--- lua/netscout/init.lua
+++ lua/netscout/init.lua
@@ -27,7 +27,7 @@ M.pathSeparator = function()
     return '/'
 end
 
-M.default_values = {
+M.default_values_per_root = {
     current_platform = "",
     current_remote   = "",
     remotes = {},
@@ -35,10 +35,30 @@ M.default_values = {
     current_dest = "",
     destinations = {}
 }
-M.values = M.default_values
 
+M.values = {}
 
 local status_file = vim.fn.stdpath('state') .. M.pathSeparator() .. 'netscout.nvim.lua'
+
+local function deepcopy(o, seen)
+  seen = seen or {}
+  if o == nil then return nil end
+  if seen[o] then return seen[o] end
+
+  local no
+  if type(o) == 'table' then
+    no = {}
+    seen[o] = no
+
+    for k, v in next, o, nil do
+      no[deepcopy(k, seen)] = deepcopy(v, seen)
+    end
+    setmetatable(no, deepcopy(getmetatable(o), seen))
+  else -- number, string, boolean, etc
+    no = o
+  end
+  return no
+end
 
 local function loadStatus()
     local test = io.open(status_file, "r")
@@ -56,10 +76,19 @@ local function saveStatus()
     test:close()
 end
 
+local function getValuesForCurrWorkspace()
+    local workdir = vim.fn.getcwd()
+    if M.values[workdir] == nil then
+        M.values[workdir] = deepcopy(M.default_values_per_root)
+    end
+    return M.values[workdir]
+end
+
 M.pickPlatform = function()
-    local platforms = { M.values.current_platform }
+    local values = getValuesForCurrWorkspace()
+    local platforms = { values.current_platform }
     for k,_ in pairs(M.config.platforms) do
-        if k ~= M.values.current_platform then
+        if k ~= values.current_platform then
             table.insert(platforms, k)
         end
     end
@@ -68,7 +97,7 @@ M.pickPlatform = function()
     { prompt = "Select platform", },
     function(platform, _)
         if platform then
-            M.values.current_platform = platform
+            values.current_platform = platform
             saveStatus()
         end
     end)
@@ -77,21 +106,23 @@ end
 M.pickRemote = function()
 
     function ListRemotes(...)
+        local values = getValuesForCurrWorkspace()
         local remotes = {}
-        for k,_ in pairs(M.values.remotes) do
+        for k,_ in pairs(values.remotes) do
             table.insert(remotes, k)
         end
         return remotes.join("\n")
     end
 
+    local values = getValuesForCurrWorkspace()
     vim.ui.input( { prompt = "Select remote: ",
     completion = 'custom, ListRemotes',
-    default = M.values.current_remote },
+    default = values.current_remote },
     function(remote)
         if remote then
-            M.values.current_remote = remote
-            if M.values.remotes[remote] == nil then
-                table.insert(M.values.remotes, remote)
+            values.current_remote = remote
+            if values.remotes[remote] == nil then
+                table.insert(values.remotes, remote)
                 saveStatus()
             end
         end
@@ -102,21 +133,23 @@ end
 M.pickDest = function()
 
     function ListDestinations(...)
+        local values = getValuesForCurrWorkspace()
         local destinations = {}
-        for k,_ in pairs(M.values.destinations) do
+        for k,_ in pairs(values.destinations) do
             table.insert(destinations, k)
         end
         return destinations.join("\n")
     end
 
+    local values = getValuesForCurrWorkspace()
     vim.ui.input( { prompt = "Select destination folder: ",
     completion = 'custom, ListDestinations',
-    default = M.values.current_dest },
+    default = values.current_dest },
     function(dest)
         if dest then
-            M.values.current_dest = dest
-            if M.values.destinations[dest] == nil then
-                table.insert(M.values.destinations, dest)
+            values.current_dest = dest
+            if values.destinations[dest] == nil then
+                table.insert(values.destinations, dest)
                 saveStatus()
             end
         end
@@ -126,12 +159,13 @@ end
 
 M.launchCommand = function()
     local commands = { }
-    if M.values.last_executed_command then
-        table.insert(commands, M.values.last_executed_command)
+    local values = getValuesForCurrWorkspace()
+    if values.last_executed_command then
+        table.insert(commands, values.last_executed_command)
     end
 
     for k,_ in pairs(M.config.commands) do
-        if k ~= M.values.last_executed_command then
+        if k ~= values.last_executed_command then
             table.insert(commands, k)
         end
     end
@@ -140,16 +174,16 @@ M.launchCommand = function()
     { prompt = "Select command:", },
     function(command, _)
         if command then
-            M.values.last_executed_command = command
+            values.last_executed_command = command
             saveStatus()
             local title = command:gsub("\"", "")
             local command_line = M.config.scripts_folder .. M.pathSeparator() .. M.config.commands[command].cmdline ..
-                                 " \"" .. M.config.platforms[M.values.current_platform].builder .. "\" \"" ..  M.config.platforms[M.values.current_platform].folder .. "\" \"" ..  M.values.current_remote .. "\" \"" .. M.values.current_dest .. "\" \"" .. vim.fn.getcwd() .. "\" " ..
+                                 " \"" .. M.config.platforms[values.current_platform].builder .. "\" \"" ..  M.config.platforms[values.current_platform].folder .. "\" \"" ..  values.current_remote .. "\" \"" .. values.current_dest .. "\" \"" .. vim.fn.getcwd() .. "\" " ..
                                  (M.config.commands[command].args or "")
 
             --print("Generated command line: " .. command_line)
 
-            vim.api.nvim_command('FloatermNew --title="' .. title .. '_on_' .. M.values.current_platform .. '" --autoclose=0 ' .. command_line)
+            vim.api.nvim_command('FloatermNew --title="' .. title .. '_on_' .. values.current_platform .. '" --autoclose=0 ' .. command_line)
         end
     end)
 end
@@ -163,9 +197,10 @@ M.cycleTerminal = function()
 end
 
 M.printStatus = function()
-    local platform = (M.values.current_platform:len() > 0) and M.values.current_platform or "No plat"
-    local remote = (M.values.current_remote:len() > 0) and M.values.current_remote or "No remote"
-    local dest = (M.values.current_dest:len() > 0) and M.values.current_dest or "No dest"
+    local values = getValuesForCurrWorkspace()
+    local platform = (values.current_platform:len() > 0) and values.current_platform or "No plat"
+    local remote = (values.current_remote:len() > 0) and values.current_remote or "No remote"
+    local dest = (values.current_dest:len() > 0) and values.current_dest or "No dest"
     return "<" .. platform  .. "> [" .. remote .. "] " .. dest
 end
 
